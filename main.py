108aaa1be9872afc9383bce6781978a2101e0af1
Willian Tomaz <willianlima28@hotmail.com>
2022-08-20 11:02:23 -0300

    fix: Fazendo pequenos ajustes na classe principal

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
 import json
-from plantonista import Plantonista
-from distribuicao import Distribuicao
 from utils.read_json_file import ReadFileJson
 
-# ------------------------------------------------------------
-from datetime import datetime, date, time, timedelta, timezone
-from calendar import monthrange
+from constants    import TIPO_DISTRIBUICAO_GENERICO, TIPO_DISTRIBUICAO_DETALHADO
+from plantonista  import Plantonista
+from distribuicao import Distribuicao
+from journey      import Journey
 
-# Pegando dia e mês atual 
-year = datetime.now().date().year
-month = datetime.now().date().month
-
-# Retorna dois valores inteiros (dia_semana, qtd_dias)
-# O primeiro valor retorna dia da semana de 0(segunda-feira) a 6(domingo) por padrão
-# O segundo valor retorna a quantidade de dias do mês 
-qtd_dias_mes = monthrange(year, month)
-
-# print(f"Quantidade de dias ({month}/{year}) => {qtd_dias_mes[1]}")
-# ------------------------------------------------------------
-
-
+# Iniciando Jornada
+journey = Journey()
+print(journey)
+print("---------------------------------")
 
 
 # TODO: Fazer o uso da classe Plantonista
 # plantonista = Plantonista(nome="", telefone="")
+
+
 
 # Pegando informações que serão utilizadas para fazer a distribuição dos plantonistas no mês
 jsonFile = ReadFileJson(file="dados/dados_post.json")
@@ -33,14 +25,14 @@ distribuicao = Distribuicao()
 
 # Fazendo a separação do mês (utilizando o mês atual) e fazendo a distribuição dos plantonistas
 for obj_mes in obj_todos_meses:
-  if obj_mes['mes'] == month:
+  if obj_mes['mes'] == journey.month:
 
     # O 'tipo-distribuicao' tem influência na distribuição dos dias dos plantonistas
     # Ela é responsável por dar mais flexibilidade na hora de passar as informações (que está no dados_post.json)
     # Quando é 'generico' significa que tem um padrão e que é repetido conforme a quantidade informada nas colunas 'revezamento-por-dias' e 'quem-inicia-o-mes' (do dados_post.json)
     # Quando é 'detalhado' precisa ser informado os dias que cada plantonista vai atuar 
-    if obj_mes['tipo-distribuicao'] == 'generico': 
-      distribuicao.getDistribuicaoGenerica(obj_mes, qtd_dias_mes[1])
+    if obj_mes['tipo-distribuicao'] == TIPO_DISTRIBUICAO_GENERICO: 
+      distribuicao.getDistribuicaoGenerica(obj_mes, journey.get_number_of_days_of_the_journey())
 
-    elif obj_mes['tipo-distribuicao'] == 'detalhado':    
+    elif obj_mes['tipo-distribuicao'] == TIPO_DISTRIBUICAO_DETALHADO:    
       distribuicao.getDistribuicaoDetalhada()
