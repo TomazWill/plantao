0f651e2343a2760bbb73021d5049acbb639f8131
Willian Tomaz <willianlima28@hotmail.com>
2022-08-21 11:00:32 -0300

    fix: Padronizando variaveis...

--- main.py
+++ main.py
@@ -19,20 +19,20 @@ print("---------------------------------")
 
 # Pegando informações que serão utilizadas para fazer a distribuição dos plantonistas no mês
 jsonFile = ReadFileJson(file="dados/dados_post.json")
-obj_todos_meses = jsonFile.get_full_data()
+list_of_months = jsonFile.get_full_data()
 
 distribution = Distribution()
 
 # Fazendo a separação do mês (utilizando o mês atual) e fazendo a distribuição dos plantonistas
-for obj_mes in obj_todos_meses:
-  if obj_mes['mes'] == journey.month:
+for month_object in list_of_months:
+  if month_object['mes'] == journey.month:
 
     # O 'tipo-distribuicao' tem influência na distribuição dos dias dos plantonistas
     # Ela é responsável por dar mais flexibilidade na hora de passar as informações (que está no dados_post.json)
     # Quando é 'generico' significa que tem um padrão e que é repetido conforme a quantidade informada nas colunas 'revezamento-por-dias' e 'quem-inicia-o-mes' (do dados_post.json)
     # Quando é 'detalhado' precisa ser informado os dias que cada plantonista vai atuar 
-    if obj_mes['tipo-distribuicao'] == GENERIC_DISTRIBUTION_TYPE: 
-      distribution.get_generic_distribution(obj_mes, journey.get_number_of_days_of_the_journey())
+    if month_object['tipo-distribuicao'] == GENERIC_DISTRIBUTION_TYPE: 
+      distribution.get_generic_distribution(month_object, journey.get_number_of_days_of_the_journey())
 
-    elif obj_mes['tipo-distribuicao'] == DETAILED_DISTRIBUTION_TYPE:    
+    elif month_object['tipo-distribuicao'] == DETAILED_DISTRIBUTION_TYPE:    
       distribution.get_detailed_distribution()
