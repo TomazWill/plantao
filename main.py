78ea6e7be33f075ea073a76da750551a060341c9
Willian Tomaz <willianlima28@hotmail.com>
2022-08-28 15:19:13 -0300

    feat: Ajustando lógica, adicionano métodos, fazendo reordenação da lista de plantonistas

--- main.py
+++ main.py
@@ -18,22 +18,16 @@ distribution = Distribution()
 current_month_object = distribution.filter_data_current_month(list_of_months, journey.month)
 
 
-
+# Faz separação dos plantonistas em uma lista para ser ordenada e utilizada na distribuição
 distribution.set_workers_of_current_month(current_month_object)
-print(distribution.get_list_of_on_duty_worker())
+# print(distribution.get_list_of_on_duty_worker())
 
 
-# for i in distribution.get_workers_of_current_month(current_month_object):
-#   on_duty_worker = OnDutyWorker(nome=f"{i['nome']}", telefone=f"{i['telefone']}")
-#   on_duty_worker.add_worker_to_list()
-#   print(f"{on_duty_worker.get_name()}")
-  # print(f"Nome: {i['nome']} | Telefone: {i['telefone']}")
-
-# TODO: Precisa começar a trabalhar em uma função que faz a alteração da ordem dos plantonistas
-
-
+# Inicia processo da distribuição das datas
 distribution.get_distribution_data_current_month(current_month_object, journey.get_number_of_days_of_the_journey())
-# print(distribution.get_on_call_workers())
+print(distribution.get_on_call_workers_by_current_month())
 
-
-
+# ETAPAS:
+# 1 - find_worker_by_name
+# 2 - delete_by_index
+# 3 - set_in_first_position_list_of_on_duty_worker
